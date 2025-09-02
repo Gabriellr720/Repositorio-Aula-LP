a5cb077b3fc1fdb9729deaf9983aa39bfe9c6c1e
Gabriellr720 <glimarocha662@gmail.com>
2025-09-02 10:44:02 -0300

    Meu segundo commit

---
+++ 01/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 1;
+        int n1 = 10;
+        int n2 = 5;
+        int soma = n + n;
+        int sub = n1 - n2;
+        int raiz = n*2; 
+    }
+}
---
+++ Calculo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repositorio-Aula-LP
+{
+    public class Calculo
+    {
+        
+    }
+}
